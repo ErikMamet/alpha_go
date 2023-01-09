d607ea3d986b90267c37168423d577261a087521
erik <erik.mamet@gmail.com>
2023-01-09 13:49:55 +0100

    dataste manager not tested, should work

--- dataset_manager.py
+++ dataset_manager.py
@@ -3,8 +3,13 @@ import numpy as np
 import os
 import os.path as osp
 import pickle
+import torchvision.transforms as T
+
+from torch.utils.data import Dataset
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+
+
 
 '''
 This dataset was sourced from [insert link] on 03/01/2023. It has been pre-processed by (see [insert final pre process file name])
@@ -32,15 +37,15 @@ def calc_nb_state_games(dataset_dir, nb_games):
     return res
 
 
-
+transf = T.ToTensor()
 
 class Go9x9_Dataset(Dataset):
-    def __init__(self, data_dir, size_of_input= 5, transform=None, target_transform=None):
+    def __init__(self, data_dir, size_of_input= 5, transform=transf, target_transform=None):
         self.data_dir = data_dir
         self.transform = transform
         self.target_transform = target_transform
         self.nb_games = len(os.listdir(data_dir))
-        self.nb_states_games = calc_nb_state_games(self.nb_games)
+        self.nb_states_games = calc_nb_state_games(data_dir, self.nb_games)
         self.size_of_input = size_of_input
 
     def calc_game_board_id(self, idx):
@@ -58,14 +63,41 @@ class Go9x9_Dataset(Dataset):
         return sum(self.nb_states_games)
 
     def __getitem__(self, idx):
+        
         game_id, board_id = self.calc_game_board_id(idx)
         board_path = osp.join(self.data_dir, "game_"+str(game_id), "state_"+str(board_id))
         with open(board_path, 'rb') as handle:
             Board, winner_color, last_player_color, next_move, number_moves_left = pickle.load(handle)
+            Board = np.reshape(Board, (9,9))
+
+        if last_player_color == -1:
+            fused_board = np.zeros((9,9), dtype='int8')
+        if last_player_color == 1:
+
+            fused_board = np.ones((9,9), dtype='int8')
+        
+        for i in range(self.size_of_input):
+            if i<self.nb_states_games[game_id]:
+                board_path = osp.join(self.data_dir, "game_"+str(game_id), "state_"+str(board_id-i))
+                with open(board_path, 'rb') as handle:
+                    Old_Board, winner_color, last_player_color, next_move, number_moves_left = pickle.load(handle)
+                    Old_Board = np.reshape(Old_Board, (9,9))
+                
+                fused_board = np.stack(( fused_board, Old_Board))
+            else :
+                fused_board = np.stack(( fused_board, np.zeros((9,9)) ))
+
+
         enocoder = OneHotEncoder(range(-2,81))
         label = winner_color*np.exp((1-number_moves_left)/9)*enocoder.fit_transform(next_move)
+
         if self.transform:
             Board = self.transform(Board)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return Board, label
+
+        return fused_board, label
+
+
+if __name__ == "__main__":
+    DATASET_PATH = "./data/mini_dataset"
+    D = Go9x9_Dataset(DATASET_PATH)
+    
